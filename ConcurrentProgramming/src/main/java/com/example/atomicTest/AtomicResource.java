d35e52c05fc185f54af64521d9d3ba53a4a5124b
baiiu <baiiu@foxmail.com>
2020-12-08 19:37:57 +0800

    sth

--- ConcurrentProgramming/src/main/java/com/example/atomicTest/AtomicResource.java
+++ ConcurrentProgramming/src/main/java/com/example/atomicTest/AtomicResource.java
@@ -12,6 +12,12 @@ public class AtomicResource {
     private final AtomicInteger number = new AtomicInteger(0);
 
     public int getNumber() {
+        // 不安全
+//        int i = number.get();
+//        number.set(0);
+//        return i;
+
+        // 安全
         return number.getAndSet(0);
     }
 
--- ConcurrentProgramming/src/main/java/com/example/atomicTest/TestAtomic.java
+++ ConcurrentProgramming/src/main/java/com/example/atomicTest/TestAtomic.java
@@ -10,39 +10,29 @@ public class TestAtomic {
     public static void main(String[] args) {
         final AtomicResource atomicResource = new AtomicResource();
 
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-
-                    for (int j = 0; j < 100; j++) {
-                        atomicResource.increment();
-                    }
-
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        for (int j = 0; j < 10; j++) {
+                            atomicResource.increment();
+                        }
                     }
                 }
-            }
-        }).start();
+            }).start();
+        }
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 5; i++) {
-                    System.out.println("atomicResource#get: " + atomicResource.getNumber());
-
-                    try {
-                        Thread.sleep(210);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 5; i++) {
+                        System.out.println("atomicResource#get: " + atomicResource.getNumber());
                     }
                 }
-            }
-        }).start();
+            }).start();
+        }
 
         try {
             Thread.sleep(3000);
